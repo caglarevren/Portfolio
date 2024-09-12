30bafcce713a3a7b94c795eba2468802a72c497d
EXT02D53263 <EXT02D53263>
2024-09-12 22:43:54 +0300

    components rearranged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 import { HeaderComponent } from './components/header/header.component';
+
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { ThemeService } from '../theme/theme.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  isDarkMode: boolean = false;
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  constructor(
+    public themeService: ThemeService,
+    private breakpointObserver: BreakpointObserver,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.isDarkMode = this.themeService.isDarkMode();
+  }
+
+  ngAfterViewInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        if (this.sidenav) {
+          if (result.matches) {
+            this.sidenav.close();
+          } else {
+            this.sidenav.open();
+          }
+          this.changeDetectorRef.detectChanges();
+        } else {
+          console.error('Sidenav is not initialized');
+        }
+      });
+  }
+
+  onDarkModeChange(isDarkMode: boolean): void {
+    this.isDarkMode = isDarkMode;
+  }
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,74 +1,42 @@
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import {
-  Component,
-  inject,
-  ViewChild,
-  AfterViewInit,
-  ChangeDetectorRef,
-} from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+import { ThemeService } from '../../../theme/theme.service';
+import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
+
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import { ThemeService } from '../../../theme/theme.service';
-import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     CommonModule,
-    MatSidenavModule,
-    MatToolbarModule,
+    LanguageSelectorComponent,
+    TranslateModule,
     MatIconModule,
     MatListModule,
     MatButtonModule,
-    TranslateModule,
-    LanguageSelectorComponent,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements AfterViewInit {
-  isDarkMode: boolean = false;
-  isSidebarOpened: boolean = false;
+export class HeaderComponent {
+  @Input() isDarkMode: boolean = false;
+  @Input() sidenav!: MatSidenav;
+  @Output() darkModeChange = new EventEmitter<boolean>();
+
   translate: TranslateService = inject(TranslateService);
+  isSidebarOpened: boolean = false;
 
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
-
-  constructor(
-    public themeService: ThemeService,
-    private breakpointObserver: BreakpointObserver,
-    private changeDetectorRef: ChangeDetectorRef
-  ) {}
-
-  ngOnInit(): void {
-    this.isDarkMode = this.themeService.isDarkMode();
-  }
-
-  ngAfterViewInit(): void {
-    this.breakpointObserver
-      .observe([Breakpoints.Handset])
-      .subscribe((result) => {
-        if (this.sidenav) {
-          if (result.matches) {
-            this.sidenav.close();
-          } else {
-            this.sidenav.open();
-          }
-          this.changeDetectorRef.detectChanges();
-        } else {
-          console.error('Sidenav is not initialized');
-        }
-      });
-  }
+  constructor(public themeService: ThemeService) {}
 
   toggleTheme(): void {
     this.themeService.toggleTheme();
     this.isDarkMode = this.themeService.isDarkMode();
+    this.darkModeChange.emit(this.isDarkMode);
   }
 }
